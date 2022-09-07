a3550c583996a88550500182a552d2a88669c62d
Abraham Quintana <abrahamqm87@gmail.com>
2022-09-07 19:08:01 +0100

    mejoro la visualización del ejemplo en el patrón Template

--- GithubOB-spring/spring-main/sesiones_20_21_22_23/spring-patterns/src/main/java/com/example/springpatterns/patterns/behavioral/template/Main.java
+++ GithubOB-spring/spring-main/sesiones_20_21_22_23/spring-patterns/src/main/java/com/example/springpatterns/patterns/behavioral/template/Main.java
@@ -3,11 +3,15 @@ package com.example.springpatterns.patterns.behavioral.template;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("\n********************* HOME *****************************");
         HtmlHome home = new HtmlHome();
         System.out.println(home.render());
+        System.out.println("**********************************************************");
+
+        System.out.println("\n********************* About Us *****************************");
 
         HtmlAboutUs about = new HtmlAboutUs();
         System.out.println(about.render());
+        System.out.println("**************************************************************");
     }
 }
